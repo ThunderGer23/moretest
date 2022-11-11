3eb2ccc1d9380bfd6623ca9666036c1f57f49229
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-11-10 23:54:14 -0600

    Rehaciendo Red DataSet_Citas pt2

--- main.py
+++ main.py
@@ -48,3 +48,63 @@ df_balanceado.sample(5)
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(df_balanceado['Cita'], df_balanceado['ieee'], stratify=df_balanceado['ieee'])
+
+X_train.head(4)
+
+bert_preprocess = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
+bert_encoder = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4")
+
+logdir = pathlib.Path(tempfile.mkdtemp())/"tensorboard_logs"
+shutil.rmtree(logdir, ignore_errors=True)
+
+"""
+"""
+
+# Bert capas
+text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+preprocessed_text = bert_preprocess(text_input)
+outputs = bert_encoder(preprocessed_text)
+
+# capas red neuronal
+l = tf.keras.layers.Dropout(0.1, name='dropout')(outputs['pooled_output'])
+l = tf.keras.layers.Dense(1, activation='sigmoid', name="output")(l)
+
+model = tf.keras.Model(inputs=[text_input], outputs =[l])
+model.summary()
+
+"""
+"""
+
+METRICS = [
+      tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+      tf.keras.metrics.Precision(name='precision'),
+      tf.keras.metrics.Recall(name='recall')
+]
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=10)
+
+"""
+"""
+
+nameRN='Citas_RN.h5'
+model.save(nameRN)
+model.evaluate(X_test,y_test)
+
+citas = [ 
+    'González, R. (2013). Costos Paramétricos - México, D.F. Instituto Mexicano de Ingeniería de Costos. D.F, México. Editorial Trillas',
+    '1.  SEP, 2011. “Programa de Estudio 2011, Guía para la Educadora, Educación  Básica Prescolar”. México, SEP.',
+    '9.  Moltó  ,  E.  Fundamentos  de  la  Educación  en  Física.  Ministerio  de  Educación, La Habana, 2003.',
+    'Bloot, S. J., & Pye, K. (2001). “Gradistat: A gran size distribution and statics package for the analysis of unconsolidated sediments”. Earth Surface Processes and Landforms, 261.',
+]
+
+def interpretacion_cita ( ref ):
+  interprete = []
+  for cita in ref:
+    if cita >=0.5 : 
+      interprete.append('cita ieee')
+    if cita<0.5 : 
+      interprete.append('cita apa')
+  return interprete
